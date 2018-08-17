877cc1f7511a12b3bb49f67ee5e1e47107b37d76
Kenney He <kenney.he@zingbox.com>
2018-08-17 11:49:59 -0700

    clean up master node in cluster

--- eks-worker-nodes.tf
+++ eks-worker-nodes.tf
@@ -160,25 +160,3 @@ resource "aws_autoscaling_group" "demo" {
     propagate_at_launch = true
   }
 }
-
-resource "aws_eks_cluster" "testing001" {
-  name            = "testing001"
-  role_arn        = "${aws_iam_role.demo-cluster.arn}"
-
-  vpc_config {
-    security_group_ids = ["${aws_security_group.demo-cluster.id}"]
-    subnet_ids         = ["${aws_subnet.demo.*.id}"]
-  }
-  lifecycle {
-    prevent_destroy = false
-  }
-  #  vpc_config {
-  #    security_group_ids = [ "sg-12113b6f"]
-  #    subnet_ids = [ "subnet-aebd40f7", "subnet-6cc97709", "subnet-64895513", "subnet-4046311b"]
-  #  }
-
-  depends_on = [
-    "aws_iam_role_policy_attachment.demo-cluster-AmazonEKSClusterPolicy",
-    "aws_iam_role_policy_attachment.demo-cluster-AmazonEKSServicePolicy",
-  ]
-}
